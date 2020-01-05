c913926ce3f388067693701e6d49b28f1b58a27a
Oscalmi <oscalmi.freitas@dcomp.ufs.br>
2020-01-04 22:17:01 -0300

    Projeto AppCommerce: Etapa 3 - Design da Tela de Produto - Concluído

--- app/src/main/java/com/example/appcommerce/ProductDetailActivity.kt
+++ app/src/main/java/com/example/appcommerce/ProductDetailActivity.kt
@@ -11,7 +11,6 @@ class ProductDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
-        setSupportActionBar(toolbar)
     }
 
 }
