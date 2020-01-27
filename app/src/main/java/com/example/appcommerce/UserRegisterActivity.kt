5f62c7ec9df75cdafbefa2be51f3a17873605833
Oscalmi <oscalmi.freitas@dcomp.ufs.br>
2020-01-27 19:15:35 -0300

    Etapa 9 - Criando a Tela de Cadastro do Usuário - Iniciado

---
+++ app/src/main/java/com/example/appcommerce/UserRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appcommerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserRegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_register)
+    }
+}
