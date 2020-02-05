d8ad0d58d7611f8c38a79848181c56413a79ecc9
Oscalmi <oscalmi.freitas@dcomp.ufs.br>
2020-02-05 20:37:12 -0300

    Etapa 12 - Criando a Listagem de Categorias de Produtos (Parte 2) - Em andamento

--- app/src/main/java/com/example/appcommerce/MainActivity.kt
+++ app/src/main/java/com/example/appcommerce/MainActivity.kt
@@ -102,7 +102,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 startActivity(intent)
             }
 
-            R.id.nav_category -> Toast.makeText(this, "Categoria", Toast.LENGTH_LONG).show()
+            R.id.nav_category -> {
+                val intent = Intent(this, ProductCategoryActivity::class.java)
+                startActivity(intent)
+            }
             R.id.nav_orders -> Toast.makeText(this, "Compras", Toast.LENGTH_LONG).show()
             R.id.nav_cart -> Toast.makeText(this, "Carrinho", Toast.LENGTH_LONG).show()
             R.id.nav_logout -> Toast.makeText(this, "Sair", Toast.LENGTH_LONG).show()
